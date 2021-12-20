21694b3a14ee262ea40e3b6e24fbdec105254804
Maxence Fortin <fortin@et.esiea.fr>
2021-12-20 14:24:58 +0100

    Coverage test improvement with LauncherTest

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -1,6 +1,12 @@
 package fr.lernejo.navy_battle;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,4 +32,18 @@ public class LauncherTest {
             Launcher.main(new String[]{"a", "b"});
         });
     }
+
+    @Test
+    void main_launch_ping_one_arg() throws Exception {
+        Launcher.main(new String[]{"1234"});
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:1234/ping"))
+            .GET()
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertThat(response.statusCode()).as("Launcher ping test one arg").isEqualTo(200);
+    }
+
 }
